531e658eed217f70c12c062d11d23aa22734cc34
Gustavo Gomez Ortega <31602394+GustavoGomez99@users.noreply.github.com>
2017-10-01 22:27:12 -0500

    Create tarea 7.c

---
+++ tarea 7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+            int num1,num2,num3;
+            printf("Introduzca numero 1:");
+    scanf("%d",&num1);
+
+            printf("Introduzca numero 2:");
+    scanf("%d",&num2);
+
+            printf("Introduzca numero 3:");
+    scanf("%d",&num3);
+    if (num1<num2 && num1<num3)
+    {
+        printf("%d",num1);
+    }
+    else if (num2<num1 && num2<num3)
+    {
+        printf("%d",num2);
+    
+       }
+    else if (num3<num2 && num3<num1)
+    {
+
+        printf("%d",num3);   
+    }
+    system("PAUSE");     
+    return 0;
+}
